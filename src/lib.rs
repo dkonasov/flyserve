cca068d6f209743bf2da015e9fabc3d2bdc90f11
Konasov Dmitriy <d.konasov@yandex.ru>
2018-07-28 22:00:40 +0300

    Removed response logging

--- src/lib.rs
+++ src/lib.rs
@@ -24,7 +24,6 @@ pub mod prelude {
         fn handle_stream(&self, mut stream: TcpStream) {
             let mut buffer = [0; 512];
             stream.read(&mut buffer).unwrap();
-            println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
             let response = "HTTP/1.1 200 OK\r\n\r\nHello, world!";
             stream.write(response.as_bytes()).unwrap();
             stream.flush().unwrap();
