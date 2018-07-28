fc03b1c0fd094d62e0cfd0e00dc82263083b2062
Konasov Dmitriy <d.konasov@yandex.ru>
2018-07-28 21:20:03 +0300

    Added dummy helloworld response to request handling

--- src/lib.rs
+++ src/lib.rs
@@ -25,6 +25,9 @@ pub mod prelude {
             let mut buffer = [0; 512];
             stream.read(&mut buffer).unwrap();
             println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+            let response = "HTTP/1.1 200 OK\r\n\r\nHello, world!";
+            stream.write(response.as_bytes()).unwrap();
+            stream.flush().unwrap();
         }
     }
 }
