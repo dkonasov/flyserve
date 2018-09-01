3a7b1b5d3a5b15e8efe7ec6e407deb6bc095e03c
Konasov Dmitriy <d.konasov@yandex.ru>
2018-09-01 18:41:04 +0300

    #3 #6 handling request with API

--- src/lib.rs
+++ src/lib.rs
@@ -6,10 +6,14 @@ pub mod prelude {
     use std::net::TcpListener;
     use std::thread;
     use self::flyserve_api::*;
+    use std::sync::Arc;
+    use std::clone::Clone;
+    use std::ops::Deref;
     
+    #[derive(Clone)]
     struct Route {
         pattern: String,
-        handlers: Vec<fn(&mut HttpRequest, &mut HttpResponse)>
+        pub handlers: Vec<fn(&mut HttpRequest, &mut HttpResponse)>
     }
 
     impl Route {
@@ -37,9 +41,11 @@ pub mod prelude {
         pub fn start(&self) {
             let addr = format!("{}:{}", self.host, self.port);
             let listener = TcpListener::bind(addr).unwrap();
+            let arc_routes = Arc::new(self.routes.clone());
             for stream in listener.incoming() {
+                let arc_cloned = Arc::clone(&arc_routes);
                 thread::spawn(|| {
-                    Server::handle_stream(stream.unwrap());
+                    Server::handle_stream(stream.unwrap(), arc_cloned);
                 });
             }
         }
@@ -53,7 +59,7 @@ pub mod prelude {
             }
             self.routes.iter_mut().find(|el| { el.pattern == pattern}).unwrap().add_handler(handler);
         }
-        fn handle_stream(mut stream: TcpStream) {
+        fn handle_stream(mut stream: TcpStream, routes: Arc<Vec<Route>>) {
             let mut buffer = Vec::new();
             stream.read_to_end(&mut buffer).unwrap();
             let mut response = HttpResponse::new();
@@ -64,6 +70,46 @@ pub mod prelude {
                 stream.write(res.to_string().as_bytes()).unwrap();
                 stream.flush().unwrap();
             }));
+            match String::from_utf8(buffer) {
+                Ok(result) => {
+                    match HttpRequest::parse(&result) {
+                        Ok(mut req) => {
+                            let mut has_some_handlers = false;
+                            for route in routes.deref() {
+                                let cmp_result = route.compare(&req.path);
+                                if cmp_result.is_some() {
+                                    if route.handlers.len() > 0 {
+                                        has_some_handlers = true;
+                                    }
+                                    let path_params = cmp_result.unwrap();
+                                    for (key, value) in path_params.iter() {
+                                        req.params.insert(key.to_string(), value.to_string());
+                                    }
+                                    for handler in route.handlers.iter() {
+                                        handler(&mut req, &mut response);
+                                    }
+                                }
+                            }
+                            if !has_some_handlers {
+                                response.status_code = 404;
+                                response.status_msg = Some("Not found".to_string());
+                                response.send();
+                            }
+                        },
+                        Err(error) => {
+                            println!("{}", error);
+                            response.status_code = 400;
+                            response.status_msg = Some("Bad request".to_string());
+                            response.send();
+                        }
+                    }
+                },
+                Err(_) => {
+                    response.status_code = 400;
+                    response.status_msg = Some("Bad request".to_string());
+                    response.send();
+                }
+            }
         }
     }
 }
